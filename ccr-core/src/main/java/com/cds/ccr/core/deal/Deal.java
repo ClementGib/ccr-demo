b18f67085a153d82f1c70bef9ab22451dc7edaa7
“Clems” <c.gibertpro@gmail.com>
2024-06-15 00:27:30 +0200

    Multi module setup; Spark isolation

---
+++ ccr-core/src/main/java/com/cds/ccr/core/deal/Deal.java
@@ -0,0 +1,18 @@
+package com.cds.ccr.core.deal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Deal implements Serializable {
+    private double amount;
+    private String baselMaturity;
+    private String legalEntity;
+    private String currency;
+}
